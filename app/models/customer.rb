80334e5040a64feaa6ba2bb06f5b8252306362f8
reyes2981 <noe.reyes92@yahoo.com>
2021-03-18 00:41:33 -0500

    setting up sign up form

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-
+    has_secure_password
     has_many :appointments 
     has_many :hairdressers, through: :appointments
     has_many :completed_appointments, through: :hairdressers, source: :appointment
