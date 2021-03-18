44ba6725c3d6b1600a50a06b362b62df5edfe97e
reyes2981 <noe.reyes92@yahoo.com>
2021-03-18 02:13:41 -0500

    added sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,11 @@ class SessionsController < ApplicationController
         session.clear
         redirect_to root_path 
     end
+
+    def create # instance variables not needed because we are not updating form
+        customer = Customer.find_by(email: params[:customer][:email])
+        if customer && customer.authenticate(params[:customer][:password])
+        else
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get '/signup' => 'customers#new'
   post '/signup' => 'customers#create'
 
-  #get '/login' =>
-  #post '/login' =>
+  get '/login' => 'sessions#new'
+  post '/login' =>'sessions#create'
 
   delete '/logout' => 'sessions#destroy'
 
