1098c8291d9d4d79a78bacc029d9277c87435bf8
reyes2981 <noe.reyes92@yahoo.com>
2021-03-18 02:23:43 -0500

    login feature live, refactoing logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,11 @@ class SessionsController < ApplicationController
     def create # instance variables not needed because we are not updating form
         customer = Customer.find_by(email: params[:customer][:email])
         if customer && customer.authenticate(params[:customer][:password])
+            session[:customer_id] = customer.id
+            redirect_to customer_path(customer)
         else
+            flash[:message] = "Incorrect login info, please try again!"
+            redirect_to '/login'
         end
     end
 end
