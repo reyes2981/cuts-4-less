fe8741b774ba8a57314f224bdd9c8c8f8d22b620
reyes2981 <noe.reyes92@yahoo.com>
2021-03-18 22:27:45 -0500

    added appointment@index nested action/route

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,16 @@
 class AppointmentsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
+    def index # nested route / setting value to @post then evalauating if it's 'nill' or "something" / if true it will continue reading doce below
+        if params[:customer_id] && @customer = Customer.find_by_id(params[:customer_id]) 
+            # confirms it's nested
+            @appointments = @customer.appointments
+        else
+            flash[:message] = "That Customer does not exist" if params[:customer]
+            @appointments = Appointment.all
+        end
+    end
+
     def new 
         @appointment = Appointment.new
     end
