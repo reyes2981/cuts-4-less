b75461fc0578c6d0e0679a20c4e2de889b2d3562
reyes2981 <noe.reyes92@yahoo.com>
2021-03-18 19:05:59 -0500

    added appointments#create action and a private appointments_param method

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,4 +4,19 @@ class AppointmentsController < ApplicationController
     def new 
         @appointment = Appointment.new
     end
+
+    def create
+        @appointment = Appointment.new
+        if @appointment.save
+            redirect_to @appointment
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def appointment_params
+        params.require(:appointment).permit(:hairdresser, :datetime)
+    end
 end
