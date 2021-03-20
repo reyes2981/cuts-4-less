7401617cf09eb4d52e1fd68d0f792d3c3afdefd9
reyes2981 <noe.reyes92@yahoo.com>
2021-03-19 23:23:14 -0500

    AddServiceIdToAppointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_041937) do
+ActiveRecord::Schema.define(version: 2021_03_20_042219) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "hairdresser_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_041937) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "datetime"
+    t.integer "service_id"
   end
 
   create_table "customers", force: :cascade do |t|
