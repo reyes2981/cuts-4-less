ba77da938225bfa8eee5eb95a8c35cd56f29b8d6
reyes2981 <noe.reyes92@yahoo.com>
2021-03-20 21:36:20 -0500

    added columns to customers / created sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_042219) do
+ActiveRecord::Schema.define(version: 2021_03_21_022823) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "hairdresser_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_03_20_042219) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
   end
 
   create_table "hairdressers", force: :cascade do |t|
@@ -39,6 +40,15 @@ ActiveRecord::Schema.define(version: 2021_03_20_042219) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
 end
