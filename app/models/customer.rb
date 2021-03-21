3e14315d9be2d6b77a20c32e761791627de873e0
reyes2981 <noe.reyes92@yahoo.com>
2021-03-20 21:57:42 -0500

    working on customer mndel to make omni authorization feature operational

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,14 @@ class Customer < ApplicationRecord
     has_many :hairdressers
     has_many :reviews_of_appointment, through: :hairdressers, source: :appointment
 
+    device :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+    :omniauthable, :omniauth_providers: [:facebook]
+
+    def self.create_from_providers_data(provider_data)
+        where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |customer|
+            customer.email = provider.data.info.email
+            customer.password = provider.data.info.password
+        end
+    end
+
 end
