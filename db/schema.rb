1798ab579f52ad8f46a0db05a402660ba9a5c116
reyes2981 <noe.reyes92@yahoo.com>
2021-03-20 21:44:44 -0500

    addedcolumn to customers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_022823) do
+ActiveRecord::Schema.define(version: 2021_03_21_024202) do
 
   create_table "appointments", force: :cascade do |t|
     t.integer "hairdresser_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_21_022823) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
+    t.string "uid"
   end
 
   create_table "hairdressers", force: :cascade do |t|
