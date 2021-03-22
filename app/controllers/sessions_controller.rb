2834430abbf3936fc525edf063a85ae3fae70d1e
reyes2981 <noe.reyes92@yahoo.com>
2021-03-22 10:14:45 -0500

    working on nested routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,9 +17,18 @@ class SessionsController < ApplicationController
     end
 
     def omniauth
-        render plain: "success"
-        
-    end
+        #find_or_create a customer using the attributes auth
+        @customer = Customer.find_or_create_by(email: auth["info"]["email"]) do |customer|
+          customer.customername = auth["info"]["first_name"]
+          customer.password = SecureRandom.hex(10)
+        end
+        if @customer.save
+          session[:customer_id] = @customer.id
+          redirect_to customer_path(@customer)
+        else
+          redirect_to '/'
+        end
+      end
 
     private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :customers do
-    resources :appointments, only: [:new, :create, :index, :destroy]
-  end
-
-  resources :hairdressers do 
-    resources :customers, only: [:new, :create, :index]
-  end
-  resources :appointments do 
-    resources :hairdressers, only: [:new, :create, :index, :destroy] #shallow routing - only nesting what is needed 
-  end
-
  # get '/auth/:provider/callback', to: 'sessions#omniauth'
 
   resources :services
@@ -25,6 +14,20 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+
+  
+  resources :hairdressers do 
+    resources :customers, only: [:new, :create, :index]
+  end
+
+  resources :customers do
+    resources :appointments, only: [:new, :create, :index]
+  end
+  
+  resources :appointments do 
+    resources :hairdressers, only: [:new, :create, :index] #shallow routing - only nesting what is needed 
+  end
+
 
 
 
